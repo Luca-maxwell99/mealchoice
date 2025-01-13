5dc63d771137ff7c3d0325692372148df3c04728
Luca Maxwell <lucamaxwell@hotmail.co.uk>
2025-01-13 12:23:36 +0000

    readies code for deploy

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+if os.path.isfile("env.py"):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,17 +24,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 ## SECRET_KEY = 'django-insecure-66!6mh(b)5!hw%$s)3k@69thu*kq&n%n+z65kddq&9o3nzxt3i'
-import os
 
-if os.path.isfile("env.py"):
-    import env
+
+
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.herokuapp.com',
+]
 
 
 # Application definition
